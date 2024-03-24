c322a9c5008ef2ca4822a3efdb337f164a476bbc
mcrymbo <croco2023@gmail.com>
2024-03-24 20:59:55 +0300

    added admin pannel

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,8 @@
-import { useState } from 'react';
-import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/about';
 import Contact from './pages/contact';
 import Navbar from './components/Navbar';
-import LandingPage from './pages/landingPage';
 import FooterComponent from './components/footer';
 import Users from './pages/users';
 import Events from './components/Events';
@@ -13,30 +11,35 @@ import SignupPage from './pages/Signup';
 import { AuthProvider } from './hooks/useAuth';
 import { ProtectedRoute } from './components/protectedRoute';
 import EventPage from './components/event/Event';
+import Admin from './Admin/Admin';
 
 function App() {
-
   return (
-    <div>
-      <Router>
-        <AuthProvider >
-        <Navbar />
-        {/* <LandingPage /> */}
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/about' element={<About />}/>
-          <Route path='/contact' element={<Contact />}/>
-          <Route path='users' element={<ProtectedRoute> <Users /> </ProtectedRoute>}/>
-          <Route path='events' element={<Events />} />
-          <Route path='login' element={<LoginPage />}/>
-          <Route path='signup' element={<SignupPage />} />
-          <Route path='postEvent' element={<EventPage />} />
-        </Routes>
-        <FooterComponent />
-        </AuthProvider>
-      </Router>
-    </div>
+    <AuthProvider>
+      <Routes>
+        <Route path='/admin/*' element={<Admin />} />
+        <Route
+          path='/*'
+          element={
+            <>
+              <Navbar />
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/contact' element={<Contact />} />
+                <Route path='/users' element={<ProtectedRoute> <Users /> </ProtectedRoute>} />
+                <Route path='/events' element={<Events />} />
+                <Route path='/login' element={<LoginPage />} />
+                <Route path='/signup' element={<SignupPage />} />
+                <Route path='/postEvent' element={<EventPage />} />
+              </Routes>
+              <FooterComponent />
+            </>
+          }
+        />
+      </Routes>
+    </AuthProvider>
   )
 }
 
-export default App
+export default App;
