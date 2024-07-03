11e1b518e99aae9dd1b00da273e017e2c05a29d1
Mcrymbo <croco2023@gmail.com>
2024-07-03 20:33:55 +0300

    added hamburg to header

--- frontend/src/components/Headers/index.jsx
+++ frontend/src/components/Headers/index.jsx
@@ -7,14 +7,14 @@ import DropdownUser from './DropdownUser';
 import DarkModeSwitcher from './DarkModeSwitcher';
 import {logo} from '../../assets'
 
-const Navbar = () => {
+const Navbar = (props) => {
 
   return (
     <header className="sticky top-0 z-999 flex w-full bg-white drop-shadow-1 dark:bg-boxdark dark:drop-shadow-none">
       <div className="flex flex-grow items-center justify-between px-4 py-4 shadow-2 md:px-6 2xl:px-11">
         <div className="flex items-center gap-2 sm:gap-4 lg:hidden">
           {/* <!-- Hamburger Toggle BTN --> */}
-          {/* <button
+          <button
             aria-controls="sidebar"
             onClick={(e) => {
               e.stopPropagation();
@@ -53,7 +53,7 @@ const Navbar = () => {
                 ></span>
               </span>
             </span>
-          </button> */}
+          </button>
           {/* <!-- Hamburger Toggle BTN --> */}
 
           <Link className="block flex-shrink-0 hidden" to="/">
