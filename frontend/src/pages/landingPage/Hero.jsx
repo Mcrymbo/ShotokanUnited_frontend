aadbf936d5d3c16f415aa19d09508953628ae334
micckey <83111475+micckey@users.noreply.github.com>
2024-07-25 15:10:47 +0300

    Carousel Slider

--- frontend/src/pages/landingPage/Hero.jsx
+++ frontend/src/pages/landingPage/Hero.jsx
@@ -1,15 +1,17 @@
 import { useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
+import Slider from 'react-slick';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faChevronLeft, faChevronRight, faCircle, faCircleDot } from '@fortawesome/free-solid-svg-icons';
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons';
 import { homePic } from '../../assets';
 import { heroimages } from '../../constants';
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
 
 const Hero = () => {
-  const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
 
-  // Update window width state on resize
   useEffect(() => {
     const handleResize = () => {
       setWindowWidth(window.innerWidth);
@@ -18,29 +20,20 @@ const Hero = () => {
     return () => window.removeEventListener('resize', handleResize);
   }, []);
 
-  // Determine how many images to display based on window width
-  const getDisplayedImages = () => {
-    if (windowWidth >= 1024) {
-      // For large screens, show 4 images
-      return heroimages.slice(currentImageIndex, currentImageIndex + 4);
-    } else {
-      // For smaller screens, show only the current image
-      return heroimages.slice(currentImageIndex, currentImageIndex + 1);
-    }
-  };
-
-  const displayedImages = getDisplayedImages();
-
-  const handlePrevClick = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + heroimages.length) % heroimages.length);
-  };
-
-  const handleNextClick = () => {
-    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % heroimages.length);
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: windowWidth >= 1024 ? 3 : 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1500,
+    prevArrow: <CustomPrevArrow />,
+    nextArrow: <CustomNextArrow />,
   };
 
   return (
-    <div className='mx-auto max-w-screen-lg py-10 px-4 sm:px-6 lg:px-8'>
+    <div className="mx-auto max-w-screen-lg py-10 px-4 sm:px-6 lg:px-8">
       <div className="flex flex-col sm:flex-row items-center w-full">
         <section className="relative flex flex-col w-full sm:w-1/2 lg:p-6">
           <div>
@@ -51,54 +44,57 @@ const Hero = () => {
               Join Shotokan United and become part of a legacy of strength, discipline, and community.
             </h2>
           </div>
-          <Link to='/auth/register'>
+          <Link to="/auth/register">
             <button className="sm:mx-auto mt-4 bg-orange-500 w-[30%] py-2 rounded-md text-white">
               Explore more
             </button>
           </Link>
         </section>
         <img
-          className="w-full h- sm:w-1/2 mt-6 sm:mt-0 sm:ml-6"
+          className="w-full h-auto sm:w-1/2 mt-6 sm:mt-0 sm:ml-6"
           src={homePic}
           alt="SUK"
           loading="lazy"
         />
       </div>
-      <div className="relative flex justify-center items-center mt-6">
-        <FontAwesomeIcon
-          icon={faChevronLeft}
-          size="2x"
-          className="absolute -left-3 sm:left-8 lg:left-0 cursor-pointer z-10"
-          onClick={handlePrevClick}
-        />
-        <div className="mx-auto max-w-screen-lg w-[100%] flex justify-center mt-4 lg:space-x-6">
-          {displayedImages.map((image, index) => (
-            <div key={index} className="w-full lg:w-1/4 px-2">
-              <img src={image.icon} alt={`Hero Image ${index}`} className='h-[50vh] w-full object-cover' loading="lazy" />
+      <div className="relative mt-6">
+        <Slider {...sliderSettings}>
+          {heroimages.map((image, index) => (
+            <div key={index} className="px-2">
+              <img src={image.icon} alt={`Hero Image ${index}`} className="h-[50vh] w-full object-cover" loading="lazy" />
               <p className="text-center mt-2">{image.title}</p>
             </div>
           ))}
-        </div>
-        <FontAwesomeIcon
-          icon={faChevronRight}
-          size="2x"
-          className="absolute -right-3 sm:right-1 lg:right-0 cursor-pointer z-10"
-          onClick={handleNextClick}
-        />
-      </div>
-      <div className="flex justify-center mt-4 space-x-2">
-        {heroimages.map((_, index) => (
-          <FontAwesomeIcon
-            key={index}
-            icon={currentImageIndex === index ? faCircleDot : faCircle}
-            size="xs"
-            className="cursor-pointer"
-            onClick={() => setCurrentImageIndex(index)}
-          />
-        ))}
+        </Slider>
       </div>
     </div>
   );
+};
+
+const CustomPrevArrow = ({ onClick }) => (
+  <div
+    className="absolute left-5 top-1/2 transform -translate-y-1/2 z-10 cursor-pointer bg-black bg-opacity-50 p-2 rounded-full"
+    onClick={onClick}
+  >
+    <FontAwesomeIcon icon={faChevronLeft} size="2x" className="text-white" />
+  </div>
+);
+
+const CustomNextArrow = ({ onClick }) => (
+  <div
+    className="absolute right-5 top-1/2 transform -translate-y-1/2 z-10 cursor-pointer bg-black bg-opacity-50 p-2 rounded-full"
+    onClick={onClick}
+  >
+    <FontAwesomeIcon icon={faChevronRight} size="2x" className="text-white" />
+  </div>
+);
+
+CustomPrevArrow.propTypes = {
+  onClick: PropTypes.func.isRequired,
+};
+
+CustomNextArrow.propTypes = {
+  onClick: PropTypes.func.isRequired,
 };
 
 export default Hero;
