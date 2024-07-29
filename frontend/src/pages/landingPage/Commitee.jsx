47922eb8960d84fa9585d92c1dd387395cc2d28a
Mcrymbo <croco2023@gmail.com>
2024-07-29 11:45:41 +0300

    updated commitee component

--- frontend/src/pages/landingPage/Commitee.jsx
+++ frontend/src/pages/landingPage/Commitee.jsx
@@ -5,15 +5,15 @@ const Commitee = () => {
   return (
     <div>
       <div className="mx-auto max-w-screen-xl text-center py-10 px-4 sm:px-6 lg:px-8">
-        <h1 className={`${styles.sectionHeadText}`}>Technical team</h1>
+        <h1 className={`${styles.sectionHeadText} md:py-4`}>Technical team</h1>
 
-        <div className="carousel carousel-center bg-neutral rounded-box max-w-full space-x-4 p-4 overflow-x-auto">
+        <div className="carousel carousel-center bg-neutral-100 rounded-box max-w-full space-x-4 p-4 overflow-x-auto">
           {commitee.map((member, index) => (
-            <div key={index} className="carousel-item w-full sm:w-auto">
+            <div key={index} className="carousel-item w-full sm:w-auto shadow-md p-2">
               <div className="flex flex-col items-center">
                 <img
                   src={member.photo}
-                  className="rounded-md w-full sm:w-60 h-auto object-cover"
+                  className="rounded-md w-full sm:w-60 md:h-60 object-cover"
                 />
                 <div className="w-full sm:w-60 mt-4 text-center">
                   <h1 className="text-lg font-semibold">{member.name}</h1>
