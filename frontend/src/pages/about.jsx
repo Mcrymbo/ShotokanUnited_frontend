863d83a6e642b70503ec0f7bbc3ec3e49e6c41dc
Mcrymbo <croco2023@gmail.com>
2024-08-11 19:49:59 +0300

    SOE optimization

--- frontend/src/pages/about.jsx
+++ frontend/src/pages/about.jsx
@@ -8,6 +8,7 @@ function About() {
     <title>History</title>
     <meta name='description'
       content="About Shotokan United Kenya. Join Shotokan United and become part of a legacy of strength, discipline, and community."/>
+    <meta name="keywords" content="about, shotokan united kenya, karate kenya" />
     <link rel="canonical" href="/about" />
   </Helmet>
   return (
